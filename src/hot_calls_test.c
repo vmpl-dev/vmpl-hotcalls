127537dba25206c5a23cfc4edff8768fa6fa2911
Benshan Mei <mbs2021@163.com>
2023-12-20 20:24:45 +0800

    Add autoconf scripts. Add hot-calls tests.

---
+++ src/hot_calls_test.c
@@ -0,0 +1,14 @@
+// src/hot_calls_test.c
+#include "hot_calls.h"
+#include <assert.h>
+
+void test_hot_calls() {
+    // Call functions defined in hot_calls.h and use assert() to verify their behavior
+    // For example:
+    // assert(my_function() == expected_result);
+}
+
+int main() {
+    test_hot_calls();
+    return 0;
+}

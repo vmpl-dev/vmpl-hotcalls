b86f1fc93a9f4bb7d6fac2535e9fd148025a6699
Benshan Mei <mbs2021@163.com>
2023-12-31 13:56:30 +0800

    Add benchmark function.

--- src/vmpl_hot_calls_test.c
+++ src/vmpl_hot_calls_test.c
@@ -20,25 +20,12 @@ int hotcalls_write(int fd, const void *buf, size_t count)
     return hotcalls_call(&args);
 }
 
-int benchmark_syscalls(hotcall_args_t *args)
+int benchmark(hotcall_args_t *args, long (*func)(hotcall_args_t *))
 {
     uint64_t start_time, end_time, total_time;
     start_time = rdtscp();
     for (int i = 0; i < NUM_ITERATIONS; i++) {
-        syscalls_call(args);
-    }
-    end_time = rdtscp();
-    total_time = end_time - start_time;
-    log_success("Average Elapsed Time: %lu ns", total_time / NUM_ITERATIONS);
-    return 0;
-}
-
-int benchmark_hotcalls(hotcall_args_t *args)
-{
-    uint64_t start_time, end_time, total_time;
-    start_time = rdtscp();
-    for (int i = 0; i < NUM_ITERATIONS; i++) {
-        hotcalls_call(args);
+        func(args);
     }
     end_time = rdtscp();
     total_time = end_time - start_time;
@@ -56,9 +43,9 @@ int main(int argc, char *argv[]) {
     log_info("setup finish ");
 
     log_info("Test syscalls before entering vmpl");
-    benchmark_syscalls(&args);
+    benchmark(&args, syscalls_call);
     log_info("Test hotcalls before entering vmpl");
-    benchmark_hotcalls(&args);
+    benchmark(&args, hotcalls_call);
 
     log_info("enter vmpl...");
     VMPL_ENTER;
@@ -66,9 +53,9 @@ int main(int argc, char *argv[]) {
     hotcalls_write(1, "hello world\n", 12);
 
     log_info("Test syscalls after entering vmpl");
-    benchmark_syscalls(&args);
+    benchmark(&args, syscalls_call);
     log_info("Test hotcalls after entering vmpl");
-    benchmark_hotcalls(&args);
+    benchmark(&args, hotcalls_call);
 
     log_info("call finish ");
     hotcalls_teardown();
